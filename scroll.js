408fed103b7a2b7683b080f1dad9b8c335983196
krivchenko74 <52877022+krivchenko74@users.noreply.github.com>
2023-10-28 17:10:39 +0500

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,18 @@
+document.querySelector('#history').addEventListener('click', function () {
+    document.querySelector('#history_block').scrollIntoView({behavior: "smooth"})
+})
+document.querySelector('#reasons').addEventListener('click', function () {
+    document.querySelector('#reasons_block').scrollIntoView({behavior: "smooth"})
+})
+document.querySelector('#about').addEventListener('click', function () {
+    document.querySelector('#about_block').scrollIntoView({behavior: "smooth"})
+})
+document.querySelector('#pros').addEventListener('click', function () {
+    document.querySelector('#pros_block').scrollIntoView({behavior: "smooth"})
+})
+document.querySelector('#ex').addEventListener('click', function () {
+    document.querySelector('#ex_block').scrollIntoView({behavior: "smooth"})
+})
+document.querySelector('#end').addEventListener('click', function () {
+    document.querySelector('#end_block').scrollIntoView({behavior: "smooth"})
+})
