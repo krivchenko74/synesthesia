91346bd99750471d0fe887f720eafa97b89cdec6
krivchenko74 <52877022+krivchenko74@users.noreply.github.com>
2023-10-28 16:34:58 +0500

    Add files via upload

--- check.js
+++ check.js
@@ -1,11 +1,11 @@
 window.onload = function () {
     console.log(window.innerWidth)
     if (window.innerWidth < 1240) {
-
-        alert("Данный тип экрана пока не поддерживается, рекомендуем использовать ПК или ноутбук, с разрешением экрана не меньше 1440 пискелей в ширину")
+        document.querySelector('body').style.display = 'none';
+        alert("Данный тип экрана пока не поддерживается, рекомендуем использовать ПК или ноутбук, с разрешением экрана не менее 1240 пискелей в ширину")
         console.log('сработало')
     }
     else {
-        document.querySelector('body').style.display = 'block';
+        document.querySelector('body').style.display = 'flex';
     }
 }
